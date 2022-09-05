33fe70752407d01f05a19cce82d29a149c0641d7
Damian Walker <dmanwalk3825@hotmail.com>
2022-09-05 15:10:02 -0500

    Added Dashboard Page and ticket table

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 import { Entry } from './pages/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 function App() {
   return (
     <div className="App">
       
       {/*<Entry/>*/}
-      <DefaultLayout>//Dashboard</DefaultLayout>
+      <DefaultLayout><Dashboard/></DefaultLayout>
     </div>
   );
 }
---
+++ src/components/ticket-table/TicketTable.comp.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+
+export const TicketTable = (tickets) => {
+  return (
+    
+    <div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Subject</th>
+                    <th>Status</th>
+                    <th>Opened Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tickets.length ?( 
+                tickets.map((row) =>(
+                <tr key={row.id}>
+                    <td>{row.id}</td>
+                    <td>{row.subject}</td>
+                    <td>{row.status}</td>
+                    <td>{row.addedDate}</td>
+                </tr>
+                ))
+                  ):(
+                  <tr>
+                    <td className="text-center" colSpan={4} >No Tickets</td>
+                </tr>
+                )}
+                
+                
+            </tbody>
+        </Table>
+    </div>
+  )
+}
